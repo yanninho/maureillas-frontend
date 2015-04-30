3522a2a1767b8df5149f36899fab19c67c1ea3a2
yanninho <y4nn5m@gmail.com>
2015-04-30 18:17:49 +0200

    fix bad GCM key

--- app/modules/common/services/config.service.js
+++ app/modules/common/services/config.service.js
@@ -4,7 +4,7 @@ angular.module('maureillasApp.common')
 
 .constant('FEEDS', {agenda:{url:'http://maureillas.surikwat.com/evenements/feed/'},annonces:{url:'http://maureillas.surikwat.com/category/annonces/feed/'}})
 
-.constant('REMOTE', {googleFeedsService:{url:'http://ajax.googleapis.com/ajax/services/feed/load',method:'JSONP',params:{v:'1.0',callback:'JSON_CALLBACK'}},pushService:{GCM:{key:'AIzaSyAvNezKfuDi-fa80rVLdlYskgQrNjCxEtw'}},maureillasService:{users:{createUser:{url:'/v1/users/{ID}/{PLATFORM}',method:'PUT'},deleteUser:{url:'/v1/users/{ID}',method:'DELETE'},getUser:{url:'/v1/users/{ID}',method:'GET'},updateUser:{url:'/v1/users/{ID}',method:'POST'}}}})
+.constant('REMOTE', {googleFeedsService:{url:'http://ajax.googleapis.com/ajax/services/feed/load',method:'JSONP',params:{v:'1.0',callback:'JSON_CALLBACK'}},pushService:{GCM:{key:'904172923113'}},maureillasService:{users:{createUser:{url:'/v1/users/{ID}/{PLATFORM}',method:'PUT'},deleteUser:{url:'/v1/users/{ID}',method:'DELETE'},getUser:{url:'/v1/users/{ID}',method:'GET'},updateUser:{url:'/v1/users/{ID}',method:'POST'}}}})
 
 .constant('URLS', {urlBackend:'http://key:dvZ3UA6BqgUsLrP82Kj5bDCu6jcsDZ35@maureillas.herokuapp.com'})
 

f4e9569a207e8c494fe8291fe5bb498aa4d1597b
yanninho <y4nn5m@gmail.com>
2015-05-06 14:27:08 +0200

    register service for push

--- app/app.js
+++ app/app.js
@@ -10,7 +10,23 @@
  * Main module of the application.
  */
 angular
-  .module('maureillasApp')
+  .module('maureillasApp', [
+                'ngCookies',
+                'ngMessages',
+                'ngResource',
+                'ngRoute',
+                'ngAnimate',
+                'ngTouch',
+                'pascalprecht.translate',
+                'ngSanitize',
+                //Application modules
+                'maureillasApp.common',
+                'maureillasApp.main',
+                'maureillasApp.feeds',
+                'maureillasApp.push',
+                'maureillasApp.server',
+                'maureillasApp.subscription'
+            ])
   .config(function ($httpProvider, $routeProvider, $locationProvider, $translateProvider, $translatePartialLoaderProvider, VIEWS) {
     $httpProvider.defaults.withCredentials = true;
     // les routes (views.json)
@@ -20,7 +36,7 @@ angular
           templateUrl: 'modules/' + keyModule + '/views/'+ page.templateHtml,
           controller: page.controller,
           resolve : {
-            'network' : function(NetworkService, DeviceService) { 
+            'network' : function($location, NetworkService, DeviceService) { 
               if (page.label != VIEWS.main.pages.networkError.label && DeviceService.isMobile())  {
                 // network error
                 if (!NetworkService.networkConnectionExist()) {   
@@ -36,7 +52,7 @@ angular
     });
     //routes inconnues >> accueil
     $routeProvider.otherwise({
-      // redirectTo: VIEWS.main.pages.accueil.path
+      redirectTo: VIEWS.main.pages.home.path
     });
 
     //translate
---
+++ app/modules/server/register.service.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name maurellaisApp.feedsList
+ * @description
+ * # feedsList
+ * Factory in the maureillasApp.
+ */
+angular.module('maureillasApp.server')
+  .factory('RegisterService', function ($q, UserService, PushService, DeviceService) {
+
+
+    var successGetUser = function(result) {
+        return result;
+    }
+
+    var getUser = function() {
+        var ID = $cookies.registerID; 
+        var promiseGetUser = UserService.get();
+        return promiseGetUser.then(successGetUser, function(error) {
+            return getUser();
+        });
+    }
+
+    var findOrCreateUser = function() {
+        return UserService.findOrCreate().then(successGetUser, function(error) {
+            return findOrCreateUser();
+        });
+    }
+
+    var successPushRegister = function(result) {
+        return findOrCreateUser();
+    }
+
+    var pushRegister = function() {
+        return PushService.register().then(successPushRegister, function(error) {
+            return pushRegister();
+        });
+    }
+
+ 	return {
+ 		registerAndStore : function() {
+            if (DeviceService.isMobile()) { 
+              // 1 - get id from cookie
+              var ID = $cookies.registerID;     
+              // 2a - cookie exist = getUser()
+              if (angular.isDefined(ID)) {
+                return getUser();
+              }
+              else {
+                return pushRegister();
+              }
+            }
+            else {
+                var deferred = $q.defer();
+                deferred.resolve('Not on mobile device');
+                console.log('not mobile device');
+                return deferred.promise;
+            }
+ 		}
+ 	}   
+});

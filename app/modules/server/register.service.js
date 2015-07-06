1f27b010bbc85d927084809c1ea6c4e009b3af5c
yanninho <y4nn5m@gmail.com>
2015-07-06 19:37:50 +0000

    update

--- app/modules/server/register.service.js
+++ app/modules/server/register.service.js
@@ -40,14 +40,14 @@ angular.module('maureillasApp.server')
             return PushService.register().then(successPushRegister, function(error) {
                 if (!NetworkService.networkConnectionExist()) {
                     var deferred = $q.defer();
-                    deferred.reject('No network connection');            
+                    return deferred.reject('No network connection');            
                 }            
                 return pushRegister();
             });
         }
         else {
             var deferred = $q.defer();
-            deferred.reject('No Push service'); 
+            return deferred.reject('No Push service'); 
         }
     }
 

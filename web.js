b49e5b89266c5b3d0af3a336101cacb93c679cea
yanninho <y4nn5m@gmail.com>
2015-07-31 15:27:09 +0200

    conf for heroku

--- web.js
+++ web.js
@@ -4,5 +4,5 @@ var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(gzippo.staticGzip("" + __dirname + "/build/dist"));
 app.listen(process.env.PORT || 5000);

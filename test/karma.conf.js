ad37cd34ce29738ad6f4928e5cab102ed451ef81
yanninho <y4nn5m@gmail.com>
2016-01-16 08:33:27 +0100

    build

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,11 +25,7 @@ module.exports = function(config) {
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
-<<<<<<< HEAD
-      'bower_components/angular-animate/angular-animate.js',
-=======
       'bower_components/angular-sanitize/angular-sanitize.js',
->>>>>>> best_version
       'bower_components/angular-translate/angular-translate.js',
       'bower_components/angular-translate-loader-partial/angular-translate-loader-partial.js',
       'bower_components/ngCordova/dist/ng-cordova.js',
